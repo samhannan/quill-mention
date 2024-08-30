c575ab8d08d9a56c8ab4baa0f0c0e525436878e4
Sam Hannan <samhannan@googlemail.com>
2024-08-30 15:05:10 +0100

    changed key constants to key codes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,10 @@
 const Keys = {
-  TAB: "Tab",
-  ENTER: "Enter",
-  ESCAPE: "Escape",
-  UP: "ArrowUp",
-  DOWN: "ArrowDown",
+  TAB: 9,
+  ENTER: 13,
+  ESCAPE: 27,
+  UP: 38,
+  DOWN: 40,
 };
+
 
 export {Keys};
